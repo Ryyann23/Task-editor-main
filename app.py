b6204335de72ccd69223ecb400e8272fc5b22828
Ryyann23 <ryanbarroso2303@gmail.com>
2025-05-03 09:53:51 -0300

    a

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# Modelo de Usuário
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
